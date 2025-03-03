bd815a40b59157f6138134877e277ed1f77245af
zzznly <zzznly@gmail.com>
2025-03-04 00:15:29 +0900

    mod: Campaign service 클래스 수정 - updateCampaign 메서드 파라미터 추가

--- src/service/campaigns/CampaignService.ts
+++ src/service/campaigns/CampaignService.ts
@@ -4,8 +4,8 @@ class CampaignService extends Service {
   async getCampaignList(page: number = 0, size: number = 25) {
     return await this.service.get<ListData<CampaignListItem>>('/campaigns', { params: { page, size } });
   }
-  async updateCampaign(id: number) {
-    return await this.service.patch(`/campaigns/${id}`);
+  async updateCampaign(id: number, data: { enabled: boolean }) {
+    return await this.service.patch(`/campaigns/${id}`, data);
   }
 }
 export default new CampaignService();
