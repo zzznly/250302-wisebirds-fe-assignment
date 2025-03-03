7197a3e61df7fc8b2c23a9631b44e03f2cdc2096
zzznly <zzznly@gmail.com>
2025-03-03 23:10:26 +0900

    mod: App.tsx - 유저정보 조회 후 전역상태로 저장되도록

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@/routes';
+import { useEffect } from 'react';
+import { useAppStore } from '@/stores';
 
 export default function App() {
+  const fetchUserInfo = useAppStore(state => state.fetchUserInfo);
+
+  useEffect(() => {
+    fetchUserInfo();
+  }, [fetchUserInfo]);
+
   return <RouterProvider router={router} />;
 }
