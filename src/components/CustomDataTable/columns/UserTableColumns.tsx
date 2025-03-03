0cf65f7d513d281da36d2e75c6df945607b5959b
zzznly <zzznly@gmail.com>
2025-03-04 01:38:33 +0900

    mod: UserTableColumns 수정 - align, valueFormatter 설정

--- src/components/CustomDataTable/columns/UserTableColumns.tsx
+++ src/components/CustomDataTable/columns/UserTableColumns.tsx
@@ -1,22 +1,44 @@
+import { formatISODateToDateTimeString } from '@/utils';
 import { Button } from '@mui/material';
-import { GridColDef } from '@mui/x-data-grid';
+import { GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
 
-export const UserTableColumns: GridColDef[] = [
-  {
-    field: 'email',
-    headerName: '아이디',
-    width: 300,
-  },
-  { field: 'name', headerName: '이름', width: 300 },
-  {
-    field: 'last_login_at',
-    headerName: '마지막 로그인 일시',
-    width: 400,
-  },
-  {
-    field: 'id',
-    headerName: '수정',
-    width: 150,
-    renderCell: params => <Button onClick={() => console.log(params.id)}>수정</Button>,
-  },
-];
+interface UserTableColumnsProps {
+  openModal: () => void;
+  onEditRowData: (row: any) => void;
+}
+export const UserTableColumns = ({ openModal, onEditRowData }: UserTableColumnsProps): GridColDef[] => {
+  const renderEditButton = (params: GridRenderCellParams<any, any>) => {
+    const onClickEditButton = () => {
+      openModal();
+      onEditRowData(params.row);
+    };
+    return <Button onClick={onClickEditButton}>수정</Button>;
+  };
+
+  return [
+    {
+      field: 'email',
+      headerName: '아이디',
+      width: 300,
+      align: 'left',
+      headerAlign: 'left',
+    },
+    { field: 'name', headerName: '이름', width: 300, align: 'left', headerAlign: 'left' },
+    {
+      field: 'last_login_at',
+      headerName: '마지막 로그인 일시',
+      width: 400,
+      align: 'left',
+      headerAlign: 'left',
+      valueFormatter: params => formatISODateToDateTimeString(params),
+    },
+    {
+      field: 'id',
+      headerName: '수정',
+      width: 150,
+      renderCell: renderEditButton,
+      align: 'center',
+      headerAlign: 'center',
+    },
+  ];
+};
