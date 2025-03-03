a0fa7efdf503a8632929653bf0065b20e97a43cb
zzznly <zzznly@gmail.com>
2025-03-03 23:09:06 +0900

    mod: 도메인별 api 메서드 타입 정의 추가

--- src/service/campaigns/CampaignService.ts
+++ src/service/campaigns/CampaignService.ts
@@ -2,7 +2,7 @@ import Service from '../Service';
 
 class CampaignService extends Service {
   async getCampaignList(page: number = 0, size: number = 25) {
-    return await this.service.get('/campaigns', { params: { page, size } });
+    return await this.service.get<ListData<CampaignListItem>>('/campaigns', { params: { page, size } });
   }
   async updateCampaign(id: number) {
     return await this.service.patch(`/campaigns/${id}`);
--- src/service/user/UserService.ts
+++ src/service/user/UserService.ts
@@ -2,10 +2,10 @@ import Service from '../Service';
 
 class UserService extends Service {
   async getUserInfo() {
-    return await this.service.get('/auth/me');
+    return await this.service.get<UserInfo>('/auth/me');
   }
   async getUserList(page: number = 0, size: number = 25) {
-    return await this.service.get('/users', { params: { page, size } });
+    return await this.service.get<ListData<UserListItem>>('/users', { params: { page, size } });
   }
   async createUser() {
     return await this.service.post('/users');
