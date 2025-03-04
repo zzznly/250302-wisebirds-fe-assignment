b188491b2d259f496ab593e432008818ed46f022
zzznly <zzznly@gmail.com>
2025-03-04 12:19:42 +0900

    chore: GlobalErrorDialog 수정

--- src/components/CustomDialog/GlobalErrorDialog.tsx
+++ src/components/CustomDialog/GlobalErrorDialog.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useAppStore } from '@/stores';
 import useDialog from '@/hooks/useDialog';
 import CustomDialog from '@/components/CustomDialog';
-import { Typography } from '@mui/material';
+import { DialogContentText } from '@mui/material';
 
 export default function GlobalErrorDialog() {
   const { error, setError } = useAppStore();
@@ -23,15 +23,14 @@ export default function GlobalErrorDialog() {
 
   return (
     <CustomDialog open={isDialogOpen} onConfirm={handleClose} onClose={handleClose}>
-      <Typography>
+      <DialogContentText>
         에러가 발생했습니다.
         <br />
         같은 현상이 발생하면 고객센터로 문의 바랍니다.
         <br />
         *고객센터
-        <br />
-        -email: helpdesk@wisebirds.ai
-      </Typography>
+        <br />- email: helpdesk@wisebirds.ai
+      </DialogContentText>
     </CustomDialog>
   );
 }
