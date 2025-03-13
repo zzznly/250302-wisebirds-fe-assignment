afbcf0dcf251b5bebbe6bae3d6185385977f2ca7
zzznly <zzznly@gmail.com>
2025-03-13 16:29:45 +0900

    refactor: user store 타입 수정

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -2,31 +2,23 @@ import { StateCreator } from 'zustand';
 import UserService from '@/service/user/UserService';
 import { USER_ROLES } from '@/constants';
 
-interface UserInfo {
-  email: string;
-  name: string;
-  company: {
-    name: string;
-  };
-}
-
 export interface UserSlice {
   userInfo?: UserInfo;
   userRole: string;
-  setUserRole: (role: string) => void;
-  setUserInfo: (userInfo: UserInfo) => void;
-  fetchUserInfo: () => Promise<void>;
+  setUserRole(role: string): void;
+  setUserInfo(userInfo: UserSlice['userInfo']): void;
+  fetchUserInfo(): Promise<void>;
 }
 
 export const createUserSlice: StateCreator<UserSlice> = (set, get) => ({
   userInfo: undefined,
   userRole: USER_ROLES[0].value,
-  setUserRole: (role: string) => set({ userRole: role }),
-  setUserInfo: (userInfo: UserInfo) => set({ userInfo }),
+  setUserRole: role => set({ userRole: role }),
+  setUserInfo: userInfo => set({ userInfo }),
   fetchUserInfo: async () => {
     try {
-      const response = await UserService.getUserInfo();
-      set({ userInfo: response.data });
+      const { data } = await UserService.getUserInfo();
+      set({ userInfo: data });
     } catch (error) {
       console.error('Error fetching user info:', error);
     }
